775023883f54a9e184b01829695306d6b9e1e2b3
Claire Dupont <claire.dupont@kedgebs.com>
2018-02-21 14:24:10 -0500

    controllers added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create(name: "Dishoom--", address: "7 Boundary St, London E2 7JE", phone_number: "07 87 99 98 00 ", category: "italian")
+Restaurant.create(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "07 87 99 98 00 ", category: "italian")
+Restaurant.create(name: "Banquise", address: "56 Rachel St, Montreal H2T 5Z3", phone_number: "07 87 99 98 00 ", category: "belgian")
+Restaurant.create(name: "Helm", address: "55 Bernard St, Montreal H2T 54R", phone_number: "07 87 99 98 00 ", category: "belgian")
+Restaurant.create(name: "Omma", address: "12 Bernard St, Montreal H2T 6T6", phone_number: "07 87 99 98 00 ", category: "chinese")
+
+
+
+

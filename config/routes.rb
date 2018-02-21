f97dbba7b177811eb7c11edb468853e5ac15375a
Claire Dupont <claire.dupont@kedgebs.com>
2018-02-21 16:43:29 -0500

    before style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:destroy, :edit, :update] do
+    resources :reviews, except: [:edit, :update, :destroy, :show, :index]
+  end
 end
